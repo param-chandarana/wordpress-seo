174b1620c56a55579910c7527ce932700b344658
Vraja Das <vraja.pro@gmail.com>
2026-06-10 13:28:23 +0300

    fix: remove ref from divider

--- packages/ui-library/src/elements/divider/index.js
+++ packages/ui-library/src/elements/divider/index.js
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import React, { forwardRef } from "react";
+import React from "react";
 
 /**
  * A horizontal line.
@@ -7,9 +7,9 @@ import React, { forwardRef } from "react";
  * @param {string} [className] The HTML class.
  * @returns {JSX.Element} The horizontal line.
  */
-const HorizontalLine = forwardRef( ( { className = "", ...props }, ref ) => (
-	<hr ref={ ref } className={ classNames( "yst-border-0 yst-border-t yst-border-slate-200", className ) } { ...props } />
-) );
+const HorizontalLine = ( { className = "", ...props } ) => (
+	<hr className={ classNames( "yst-border-0 yst-border-t yst-border-slate-200", className ) } { ...props } />
+);
 
 HorizontalLine.displayName = "HorizontalLine";
 
@@ -18,25 +18,25 @@ HorizontalLine.displayName = "HorizontalLine";
  * @param {string} [className] The HTML class.
  * @returns {JSX.Element} The divider.
  */
-const Divider = forwardRef( ( {
+const Divider = ( {
 	children = null,
 	className = "",
 	...props
-}, ref ) => {
+} ) => {
 	// Without content the divider is a single horizontal line, acting as a `separator`.
 	if ( ! children ) {
-		return <HorizontalLine ref={ ref } className={ className } { ...props } />;
+		return <HorizontalLine className={ className } { ...props } />;
 	}
 
 	// With content the line is split so the children sit centered between the two halves.
 	return (
-		<div ref={ ref } className={ classNames( "yst-flex yst-items-center", className ) } { ...props }>
+		<div className={ classNames( "yst-flex yst-items-center", className ) } { ...props }>
 			<HorizontalLine className="yst-grow" />
 			{ children }
 			<HorizontalLine className="yst-grow" />
 		</div>
 	);
-} );
+};
 
 Divider.displayName = "Divider";
 
